2fdcfa6e3db8ec9c1489da8dc85e30d90338657b
Kyra Edwards <kedward8@oswego.edu>
2024-04-21 13:11:34 -0400

    added categories to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Category.create(name: "Sports")
+Category.create(name: "Beauty")
+Category.create(name: "Hobbies")
